96ed6c5b474b1def7b38cdda40ea3c815ed7e98a
weimin yang <yangwm1983@gmail.com>
2019-07-29 08:53:38 -0700

    modify

--- server.js
+++ server.js
@@ -80,8 +80,8 @@ app.get("/scrape", function(req, res) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(html);
     // Now, we grab every h2 within an article tag, and do the following:
+    var articles =[]
     $("article").each(function(i, element) {
-
       // Save an empty result object
       var result = {};
 
@@ -98,24 +98,29 @@ app.get("/scrape", function(req, res) {
       result.summary = summary;
       result.link = "https://www.nytimes.com" + $(this).find("a").attr("href");
 
-      // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
-      var entry = new Article(result);
+     articles.push(result)
 
-      // Now, save that entry to the db
-      entry.save(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        // Or log the doc
-        else {
-          console.log(doc);
-        }
-      });
 
+     
     });
       // Tell the browser that we finished scraping the text
+      for( let i=3;i<13;i++){
+        
+        
+        var entry = new Article(articles[i]);
+    
+        // Now, save that entry to the db
+        entry.save(function(err, doc) {
+          // Log any errors
+          if (err) {
+            console.log(err);
+          }
+          // Or log the doc
+          else {
+            console.log(doc);
+          }
+        });
+      }
        res.send("Scrape Complete");
 
   });
